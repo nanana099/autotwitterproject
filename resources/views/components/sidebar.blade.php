597fe66ba75339ca92069dcbb4cb98c861cb41bf
アキナミ <>
2019-12-03 19:11:17 +0900

    サイドバーの改行始まり位置を調整

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,12 +1,12 @@
 <div id="sidebar">
     <h2 class="c-side-menu__title">{{Auth::user()->name}}さん</h2>
     <ul class="c-side-menu">
-        <li class="c-side-menu__list"><a href="{{route('mypage.monitor')}}" class="c-side-menu__link"><i class="fas fa-desktop u-mr-2"></i>アカウント一覧・稼働状況</a></li>
-        <li class="c-side-menu__list"><a href="{{route('mypage.reserve')}}" class="c-side-menu__link"><i class="far fa-clock u-mr-2"></i>ツイート予約</a></li>
-        <li class="c-side-menu__list"><a href="{{route('mypage.account')}}" class="c-side-menu__link"><i class="fas fa-user-slash u-mr-2"></i>アカウント削除</a></li>
-        <li class="c-side-menu__list"><a href="{{route('mypage.setting')}}" class="c-side-menu__link"><i class="fas fa-cog u-mr-2"></i>設定</a></li>
-        <li class="c-side-menu__list"><a href="{{route('user.editinfo')}}" class="c-side-menu__link"><i class="fas fa-user-tag u-mr-2"></i>ユーザー情報編集</a></li>
-        <li class="c-side-menu__list"><a href="{{route('user.editpass')}}" class="c-side-menu__link"><i class="fas fa-key u-mr-2"></i>パスワード変更</a></li>
-        <li class="c-side-menu__list"><a href="{{route('user.retire')}}" class="c-side-menu__link"><i class="far fa-times-circle u-mr-2"></i>退会</a></li>
+        <li class="c-side-menu__list"><i class="fas fa-desktop u-mr-2 c-side-menu__icon"></i><a href="{{route('mypage.monitor')}}" class="c-side-menu__link">アカウント一覧・稼働状況</a></li>
+        <li class="c-side-menu__list"><i class="far fa-clock u-mr-2"></i><a href="{{route('mypage.reserve')}}" class="c-side-menu__link">ツイート予約</a></li>
+        <li class="c-side-menu__list"><i class="fas fa-user-slash u-mr-2"></i><a href="{{route('mypage.account')}}" class="c-side-menu__link">アカウント削除</a></li>
+        <li class="c-side-menu__list"><i class="fas fa-cog u-mr-2"></i><a href="{{route('mypage.setting')}}" class="c-side-menu__link">設定</a></li>
+        <li class="c-side-menu__list"><i class="fas fa-user-tag u-mr-2"></i><a href="{{route('user.editinfo')}}" class="c-side-menu__link">ユーザー情報編集</a></li>
+        <li class="c-side-menu__list"><i class="fas fa-key u-mr-2"></i><a href="{{route('user.editpass')}}" class="c-side-menu__link">パスワード変更</a></li>
+        <li class="c-side-menu__list"><i class="far fa-times-circle u-mr-2"></i><a href="{{route('user.retire')}}" class="c-side-menu__link">退会</a></li>
     </ul>
 </div>
