4325beafc68d55dbfb90378931a7d6b57818f0e9
Arknanana <nananabaito@yahoo.co.jp>
2022-09-01 09:07:42 +0900

    「アカウント削除」という文言を「Twitterアカウント登録削除」に変更

--- resources/views/mypage/account.blade.php
+++ resources/views/mypage/account.blade.php
@@ -1,11 +1,11 @@
 @php
-    $description = "Twitter自動化ツール「神ったー」に登録したアカウントを削除します。";
-    $title = "アカウント削除";
+$description = "Twitter自動化ツール「神ったー」に登録したアカウントを削除します。";
+$title = "Twitterアカウント登録削除";
 @endphp
 @extends('layouts.app')
 
 @section('content')
-<h2 class="c-title">アカウント削除</h2>
+<h2 class="c-title">Twitterアカウント登録削除</h2>
 <section class="p-section">
     <account-list></account-list>
 </section>
